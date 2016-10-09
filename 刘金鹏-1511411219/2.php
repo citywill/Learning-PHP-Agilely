c1c1368c7b0682268eb3e5703c1e38375752029f
ASentry <954533781@qq.com>
2016-10-09 18:20:51 +0800

    设置新的界面

--- 刘金鹏-1511411219/2.php
+++ 刘金鹏-1511411219/2.php
@@ -1,8 +1,8 @@
 <?php
-header("Content-type:text/html;charset=gb2312");
-$str=$_POST['title']. ", ".$_POST['content'] . "\n";
-$fh=fopen('./3.txt','a');
-fwrite($fh,$str);
+header("Content-type:text/html;charset=utf-8");
+$str = $_POST['title'] . ", " . $_POST['content'] . "\n";
+$fh = fopen('./3.txt', 'a');
+fwrite($fh, $str);
 fclose($fh);
 echo '留言成功！</br>';
 ?>
